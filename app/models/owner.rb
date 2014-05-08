00966162eb8f4f21fb86759dce3dcf3cbdd8480c
Suzanne Atkinson <suzanneatkinson@gmail.com>
2014-05-08 12:46:30 -0400

    dynamic home page

---
+++ app/models/owner.rb
@@ -0,0 +1,24 @@
+class Owner
+
+
+def name
+	name = "Suzanne Atkinson"
+end
+
+def birthdate
+	birthdate = Date.new(1968, 11,19)
+	
+end
+
+
+def countdown
+	today = Date.today
+	birthday = Date.new(today.year, birthdate.month, birthdate.day)
+	if birthday > today 
+		countdown = (birthday - today).to_i
+	else
+		countdown = (birthday.next_year - today).to_i
+	end
+end
+
+end
